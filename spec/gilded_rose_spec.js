25fde67837792a9cfee1f8e0fed8482d62be0589
Lisa Brunda <if24b009@technikum-wien.at>
2025-06-10 22:11:22 +0200

    2. Testcase green: Für generelle Items -> quality und sell_in sollten um 1 niedriger werden

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,16 @@
-describe("Gilded Rose", function() {
+describe("General logic", function() {
 
   it("should foo", function() {
     items = [ new Item("foo", 0, 0) ];
     update_quality();
     expect(items[0].name).toEqual("foo");
   });
+
+   it("quality and sell_in should decrease by 1 for general items", function() {
+      items = [new Item("GeneralItem", 4, 20)];
+      update_quality();
+      expect(items[0].sell_in).toEqual(3);
+      expect(items[0].quality).toEqual(19);
+    });
 
 });
