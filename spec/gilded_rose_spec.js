61bb527e069bf88b9eaec4a2dbc8d044ca17c530
Lisa Brunda <if24b009@technikum-wien.at>
2025-06-10 22:44:38 +0200

    Backstage: 3. Test green: bei einem Anstieg um 2 in der Quality bei 10 übrigen Tagen, darf 50 nicht überschritten werden

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -97,4 +97,10 @@ describe("Backstage passes", function() {
     expect(items[0].quality).toEqual(34);
   });
 
+  it("quality should increase to 50 when 10 days are left", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+  });
+
 });
