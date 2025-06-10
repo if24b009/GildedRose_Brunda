cc3ae90058a9fd343d35d0371b34a0322ba6ff6e
Lisa Brunda <if24b009@technikum-wien.at>
2025-06-10 22:29:54 +0200

    Aged Brie: 2. testcase nach Ablauf von sell_in soll solang quality unter 50 ist um 2 increased bzw auf 50 werden

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -45,4 +45,10 @@ describe("Aged Brie", function() {
     expect(items[0].quality).toEqual(50);
   });
 
+  it("quality should increase by 2 after sell_in by date has passed", function() {
+    items = [ new Item("Aged Brie", -1, 49) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+  });
+
 });
